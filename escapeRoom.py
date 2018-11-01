e3d338058660d6d28d53bf59a42847cddfde6797
cecilzha <44153388+cecilzha@users.noreply.github.com>
2018-11-01 11:28:54 +0630

    Add files via upload

---
+++ escapeRoom.py
@@ -0,0 +1,4 @@
+rooms = ((1,2,3),(0),(0),(0,4),(3,5),(4),(3,7),(6,8),(7,9),(8))
+roomName = ("Basement","Dusty Painting","Other Corner of basement","Kitchen","Living Room","Secret Room","Dark Room", "Garage", "Driveway","Outside")
+
+print(type(rooms))
