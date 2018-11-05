bd0e4be577a90c7bbed264af58881cdab45a2db3
P3ter278 <44257699+P3ter278@users.noreply.github.com>
2018-11-05 11:26:33 +0630

    Update escapeRoom.py

--- escapeRoom.py
+++ escapeRoom.py
@@ -1,4 +1,45 @@
-rooms = ((1,2,3),(0),(0),(0,4),(3,5),(4),(3,7),(6,8),(7,9),(8))
-roomName = ("Basement","Dusty Painting","Other Corner of basement","Kitchen","Living Room","Secret Room","Dark Room", "Garage", "Driveway","Outside")
+# An Adventure Game Template
 
-print(type(rooms))
+def main():
+
+    rooms = ((1,2,3),(0,),(4,6),(5,3),(4,),(3,7),(6,8),(7,9))
+    roomnames=('Basement','Dusy Painting','Basement part 2','Kitchen',
+               'Living Room','Secret Room','Dark Room','Garage','Driveway','Outside')
+    items=['','1/11/16','','Crowbar','Motorbike Key','','Candle','','','']
+    inventory=[]
+
+
+    currentroom = 0
+    nextroom = 0
+    getroom = ''
+    
+    while (getroom != 'x'):
+
+        print('Current room #{0}: {1}'.format(currentroom,roomnames[currentroom]))
+
+        # If there is an item in this room, add it to the inventory
+         # and remove it from the items list replacing it with ''
+
+        if items[currentroom]!='':
+            print("You found a {0}!".format(items[currentroom]))
+            inventory.append(items[currentroom])
+            items[currentroom]=''
+        
+        
+        print('You can move to these rooms:')
+
+        for i in rooms[currentroom]:
+            print("\t Room #{0}: {1}".format(i,roomnames[i]))
+            
+
+        getroom = input('Where to? ')
+
+        if getroom.isnumeric():
+            nextroom=int(getroom)
+            
+        if nextroom in rooms[currentroom]:
+            currentroom=nextroom
+
+        print(inventory)
+ 
+main()
